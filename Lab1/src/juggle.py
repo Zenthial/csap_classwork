b6f293e866a00262da432a24fddb45d7c7cefcfd
Zenthial <iamzenthial@gmail.com>
2021-08-29 14:46:27 -0400

    working juggle func

--- Lab1/src/juggle.py
+++ Lab1/src/juggle.py
@@ -1,10 +1,9 @@
 def juggle(msg):
     length = len(msg)
     mid = length // 2 -1
-    new_msg: list[str] = []
+    new_msg: list[str] = [''] * length
     for i in range(length-1, -1, -1):
         new_pos = mid-i # if mid >= i else i - mid;
-        print(i, mid, new_pos)
-        new_msg.insert(new_pos, msg[i])
+        new_msg[new_pos] = msg[i]
     
     return ''.join(new_msg)
