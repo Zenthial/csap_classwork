50193e8a8166d4f80aa8d8e3e8636df72ddcb061
Zenthial <iamzenthial@gmail.com>
2021-09-22 09:49:17 -0400

    tuesday lec

--- Java/Lectures/Lecture1/main.java
+++ Java/Lectures/Lecture1/main.java
@@ -1,9 +1,5 @@
 class Main {
     public static void main(String[] args) {
-        String test = "test";
-        int number = 1;
-        double otherNum = 1.0;
-
-        System.out.println(test + String.valueOf(number) + String.valueOf(otherNum));
+        
     }
 }
