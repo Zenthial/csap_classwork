14e1698ca283c575b105d0592a4d3e2d225c58d8
Zenthial <iamzenthial@gmail.com>
2021-09-26 12:52:05 -0400

    document classes

--- Python/Projects/Project1-Movies/src/classes.py
+++ Python/Projects/Project1-Movies/src/classes.py
@@ -3,18 +3,31 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Rating:
+    """The Rating Dataclass stores all the needed datafields from the ratings files"""
     tconst: str
+    """The id of the movie"""
     average_rating: float
+    """The average rating given to the movie by reviewers"""
     num_votes: int
+    """The number of unique ratings"""
 
     
 @dataclass(frozen=True)
 class Movie:
+    """The Movie Dataclass stores all needed datafields from the movie files"""
     tconst: str
+    """The id of the movie"""
     title_type: str
+    """Type of the movie/show/video game"""
     primary_title: str
+    """The primary title that is shown in publication"""
     original_title: str
+    """A previous working title, if there was one"""
     start_year: int
+    """The year the Movie was created"""
     end_year: int
+    """The end year, if the Movie/Show lasted more than one year"""
     runtime: int
+    """Length of the Movie, in minutes"""
     genres: list[str]
+    """List of all the different related genres associated with the Movie"""
